3f9c39e49b449a3c5aaaa823c4fff432dd93a8f9
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-09-10 10:36:16 +0530

    Created Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 // import React from 'react'
 
+import Header from "./components/Header"
+
 const App = () => {
   return (
-    <div className="">App</div>
+    <div >
+       <Header/>
+    </div>
   )
 }
 
