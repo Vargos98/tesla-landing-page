c9c58e22ef181eed808c81493ee3091a3fc9d9fb
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-09-10 14:26:35 +0530

    Update CyberTruck.jsx

--- src/Components/CyberTruck.jsx
+++ src/Components/CyberTruck.jsx
@@ -1,16 +1,23 @@
 // import React from 'react'
 import Cyber from '../assets/CyberTruck.png';
+
 const CyberTruck = () => {
   return (
-    <div className="header-container bg-CyberTruck h-screen w-full bg-center bg-cover relative">
-    
-        <img className='w-[520px] left-[50%] translate-x-[95%] py-14'  src={Cyber} alt={Cyber} />
-        <div className='absolute top-[620px] flex flex-col left-[50%] translate-x-[-50%] gap-4 lg:flex-row'>
-      <button className='bg-[#1f1e1ee5] px-24 py-2 text-[14px] uppercase whitespace-nowrap undeline underline-offset-4 un text-gray-400 font-bold  hover:bg-[#3a3939e5]'>Order Now</button>
-      <button className='bg-[#1f1e1ee5] px-24 py-2 text-[14px] whitespace-nowrap uppercase text-gray-400 font-bold  hover:bg-[#3a3939e5]'>Demo Drive</button>
-    </div>
-    </div>
-  )
-}
+    <div className="header-container bg-CyberTruck h-screen w-full bg-center bg-cover flex items-center justify-center relative">
+      {/* Centered CyberTruck Image */}
+      <img className="w-[80%] mt-[-320px] max-w-[520px] object-contain" src={Cyber} alt="CyberTruck" />
 
-export default CyberTruck
+      {/* Buttons */}
+      <div className="absolute bottom-16 flex flex-col left-1/2 transform -translate-x-1/2 gap-4 lg:flex-row">
+        <button className="bg-[#1f1e1ee5] px-24 py-2 text-[14px] uppercase whitespace-nowrap underline underline-offset-4 text-gray-400 font-bold hover:bg-[#3a3939e5]">
+          Order Now
+        </button>
+        <button className="bg-[#1f1e1ee5] px-24 py-2 text-[14px] uppercase text-gray-400 font-bold hover:bg-[#3a3939e5]">
+          Demo Drive
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CyberTruck;
